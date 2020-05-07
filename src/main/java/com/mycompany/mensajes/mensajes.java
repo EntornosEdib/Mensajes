f3c6b60639f5f056cae2b49b94c9c90f3f14e85a
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-05-07 12:36:12 +0200

    Version inicial aplicacion Mensajes para Entornos 2019-2020

---
+++ src/main/java/com/mycompany/mensajes/mensajes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajes;
+
+/**
+ *
+ * @author Rafael
+ */
+public class mensajes {
+     public static void main(String args[]){
+        //int a=3;
+        //int b=2;
+        //int res = suma(a,b);
+        System.out.println("Hola como estás.");
+        }
+}
