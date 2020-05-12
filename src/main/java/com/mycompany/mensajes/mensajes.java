8933ad9e69eeb2c3e4c06ba640c0cfd49369b23e
WoltersoderWolltersnicht <61188877+WoltersoderWolltersnicht@users.noreply.github.com>
2020-05-12 23:24:17 +0200

    Solicitar telefono de contacto
    Metodo telefonotoString que solicita dos Telefonos con 9 dígitos y los devuelve con un formato:
    999-999-999/888-888-888

--- src/main/java/com/mycompany/mensajes/mensajes.java
+++ src/main/java/com/mycompany/mensajes/mensajes.java
@@ -20,4 +20,34 @@ public class mensajes {
         String name = sc.nextLine();
         System.out.println("Hola como estás "+name  );
         }
+     
+     public static String telefonotoString(int Telefono1, int Telefono2) {
+   	  String end="";	 
+   	  if ((int)(Math.log10(Telefono1)+1)!=9)  {
+   		  throw new IllegalArgumentException ("Number 1 must have 9 digits"); 
+   	  }
+   	  if ((int)(Math.log10(Telefono2)+1)<9) {
+   		  throw new IllegalArgumentException ("Number 2 must have 9 digits"); 
+   	  } 
+	
+   	  String Telefono =Integer.toString(Telefono1);
+   	  for (int x = 0; x<2; x++) {
+   		  for (int i = 3; i <=9; i=i+3) {
+   			  if (i<=3) {
+   				  end= end+""+Telefono.substring(0, 3);
+   				  Telefono=Telefono.substring(3,Telefono.length() );
+   			  }else {
+   				  try {
+   					  end= end+"-"+Telefono.substring(0, 3);
+   					  Telefono=Telefono.substring(3,Telefono.length() );
+   				  }catch(Exception e){Telefono=null;}
+   			  }
+  	
+   		  }
+   		  Telefono=Integer.toString(Telefono2);    	
+   		  end=end+"/";
+   	  }
+   	  end=end.substring(0,end.length()-1);
+   	  return end; 
+	}
 }
