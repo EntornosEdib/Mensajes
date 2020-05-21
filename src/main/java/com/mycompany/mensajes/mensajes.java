1e05db20994786997e069aa29291cc484a5b9a2d
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-05-21 12:19:14 +0200

    Version con tel y correo ok

--- src/main/java/com/mycompany/mensajes/mensajes.java
+++ src/main/java/com/mycompany/mensajes/mensajes.java
@@ -30,7 +30,11 @@ public class mensajes {
        
        //Metodo abs de la Clase Math para evitar que sea negativo
        Telefono=telefonotoString(Math.abs(Tel1),Math.abs(Tel2));
-       System.out.println(Telefono+" Guardado Correctamente");       
+       System.out.println(Telefono+" Guardado Correctamente");
+         //Correo Electronico
+       System.out.println("Cual es tu correo electronico");
+       email.correo_electronico();
+           
     } 
     
     public static String telefonotoString(int Telefono1, int Telefono2) {
